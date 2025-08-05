c386e59bec163803b68bfeb05ed4ca33c8b17576
Sahyadrishinde <shindesahyadri5@gmail.com>
2025-08-05 17:07:48 +0530

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Counter
+{
+static int count=0;
+public Counter()
+{
+count++;
+System.out.println(count);
+}
+public static void main(String[] args)
+{
+Counter c1=new Counter();
+Counter c2=new Counter();
+Counter c3=new Counter();
+}
+}
